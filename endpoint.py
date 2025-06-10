819e4f100a7e921720d4ad68254d4d9d8525bdc1
Aiden <40171727+Aidenedia@users.noreply.github.com>
2025-06-10 02:16:44 +0100

    Create endpoint.py

---
+++ endpoint.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+last_command = "none"
+app = Flask(__name__)
+@app.route('/move', methods=['POST','GET'])
+
+def move():
+    valid_commands = ["forward","back","left","right","jump"]
+    global last_command
+    if request.method == 'POST':
+        last_command = request.data.decode("utf-8").strip()
+        return f"Command received: {last_command}"
+    return f"Last command: {last_command}"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
